38e32a1e3d590659c25a72ff89eee201d97365e3
Riskiperdana14 <120175911+Riskiperdana14@users.noreply.github.com>
2024-03-20 13:30:42 +0800

    feat/note3 - fix backend
    change model name, fix variabel

--- backend/src/models/mainModel.js
+++ backend/src/models/mainModel.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const mainSchema = new mongoose.Schema(
   {
-    tittle: {
+    title: {
       type: String,
       required: true,
     },
-    content: {
+    desc: {
       type: String,
       required: true,
     },
@@ -14,6 +14,6 @@ const mainSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Admin = mongoose.model("Admin", mainSchema);
+const Note = mongoose.model("Note", mainSchema);
 
-export default Admin;
+export default Note;
